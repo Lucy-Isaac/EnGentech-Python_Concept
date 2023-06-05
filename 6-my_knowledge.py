5b4067cb8f8c78b0b2f2a91fcbfd2d656ec56fb6
Lucy-Isaac <134278473+Lucy-Isaac@users.noreply.github.com>
2023-06-05 15:50:53 -0700

    Create 6-my_knowledge.py

---
+++ 6-my_knowledge.py
@@ -0,0 +1,9 @@
+Defination1="Information can be passed into functions as arguments."
+Defination2="Arguments are specified after the function name, inside the parentheses." 
+Definition3="You can add as many arguments as you want, just separate them with a comma."
+Example="Examples are: "
+Example1="def greet (func): greeting = func (""Hi, I am created by a function passed as an argument."")"
+Example2="def shout (text): return text.upper ()"
+Example3="def whisper (text): return text.lower ()"
+print("What is Arugument Passing?\n")
+print((Defination1 + "\n" + Defination2 + "\n" + Definition3 + "\n" + Example + "\n" + "1."+ Example1 + "\n" + "2." + Example2 + "\n" + "3." + Example3))
