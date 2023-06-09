074b1d2c2a178dc50614e00ae29d2bf0f6256914
Lucy Isaac <134278473+Lucy-Isaac@users.noreply.github.com>
2023-06-09 09:33:22 -0700

    Create 2-calculator_basic.py

---
+++ 0x01-flow_control/2-calculator_basic.py
@@ -0,0 +1,28 @@
+def add_num(num1, num2):
+    print(num1 + num2)
+
+
+def sub_num(num1, num2):
+    print(num1 - num2)
+
+
+def mult_num(num1, num2):
+    print(num1 * num2)
+
+
+def div_num(num1, num2):
+    print(num1 / num2)
+
+
+def rem_num(num1, num2):
+    print(num1 % num2)
+
+
+num1=int(input("Enter a  first value: "))
+num2=int(input("Enter a  second value: "))
+
+add_num(num1, num2)
+sub_num(num1, num2)
+mult_num(num1, num
+div_num(num1, num2)
+rem_num(num1, num2)
