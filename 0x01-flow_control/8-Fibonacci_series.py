b881056600a43c234ec286d13ea8045ee325a6ea
Lucy Isaac <134278473+Lucy-Isaac@users.noreply.github.com>
2023-06-14 11:10:04 +0100

    Create 8-Fibonacci_series.py

---
+++ 0x01-flow_control/8-Fibonacci_series.py
@@ -0,0 +1,12 @@
+def fib_series(num):
+    a=0
+    b=1
+    c=a+b
+    for x in range(num):
+        if x <= 1:
+            print(x, end=" ")
+        else:
+            c = a + b
+            a, b = b, c
+            print(c, end=" ")
+fib_series(num)
